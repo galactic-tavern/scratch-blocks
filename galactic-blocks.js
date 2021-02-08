ab4aecc924ad068a919efdcce65dc6d1a8571ff5
rene van der ark <rene.vanderark@kb.nl>
2021-02-08 22:28:33 +0100

    save and load mapItem code.

--- galactic-blocks.js
+++ galactic-blocks.js
@@ -3,7 +3,12 @@ var workspace = null;
 
 window.addEventListener("open-blocks", function(ev) {
     document.getElementById("blocklyWrap").style.display = "block";
-    
+    var mapItem = ev.detail.mapItem;
+    var sprite = ev.detail.sprite;
+    document.getElementById("sprite-prev").src = sprite.images[mapItem.costumeIdx];
+    document.getElementById("xpos-span").innerHTML = mapItem.x;
+    document.getElementById("ypos-span").innerHTML = mapItem.y;
+
     if (workspace === null) {
         workspace = Blockly.inject('blocklyDiv', {
             comments: true,
@@ -31,25 +36,30 @@ window.addEventListener("open-blocks", function(ev) {
             }
         });
 
-        function clearAndHide() {
-            workspace.clear();
-            document.getElementById("blocklyWrap").style.display = "none";
-        }
 
-        document.getElementById("blocklyCancel").onclick = clearAndHide;
 
-        document.getElementById("blocklySave").onclick = function() {
-            window.dispatchEvent(new CustomEvent("save-blocks", {detail: {
-                code: Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(workspace)),
-                
-            }}));
-            clearAndHide();
-        }
     } else {
         workspace.updateToolbox(ev.detail.toolbox);
     }
-    document.getElementById("sprite-prev").src = ev.detail.sprite.images[ev.detail.mapItem.costumeIdx];
-    document.getElementById("xpos-span").innerHTML = ev.detail.mapItem.x;
-    document.getElementById("ypos-span").innerHTML = ev.detail.mapItem.y;
 
+    if (mapItem.code) {
+        var xml = Blockly.Xml.textToDom(mapItem.code);
+        Blockly.Xml.domToWorkspace(xml, workspace);
+    }
+
+    function clearAndHide() {
+        workspace.clear();
+        document.getElementById("blocklyWrap").style.display = "none";
+    }
+
+    document.getElementById("blocklyCancel").onclick = clearAndHide;
+
+    document.getElementById("blocklySave").onclick = function() {
+        window.dispatchEvent(new CustomEvent("save-blocks", {detail: {
+            code: Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(workspace)),
+            mapItem: mapItem
+        }}));
+        clearAndHide();
+    }
+    
 });
